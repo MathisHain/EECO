c46a25997bedac91a2816f72c470b4cbbee4ca7d
ffripiat <100421987+ffripiat@users.noreply.github.com>
2024-09-26 19:47:19 +0200

    Update CO2atm_ode_20240926.m

--- CO2atm_ode.m
+++ CO2atm_ode.m
@@ -14,7 +14,7 @@ area_h  = .15*A_tot;                %Area high lat ocean m2
 area_l  = .85*A_tot;                %Area low lat ocean m2 
 V_h     = area_h*mix_h*1027;         %Mass of high-latitude box (Area high is 15%) Kg (m3*1027Kg/m3)
 V_l     = area_l*mix_l*1027;        %Mass of low-latitude box (Area low is 85%) Kg (m3*1027Kg/m3)
-V_d     = (V_tot - V_h - V_l)*1027;  %Mass of deep box (calculated by difference) (m3*1027Kg/m3)
+V_d     = (V_tot - V_h - V_l);  	%Mass of deep box (calculated by difference) (m3*1027Kg/m3)
 V_a     =1.773*10^20;                % Mass of atmospheric box [mol] 
 %V_a     = 8.69e18*1.25;             %Mass TROPOSPHERE (calculated) (m3*1.25Kg/m3)
 
@@ -22,78 +22,61 @@ KE_l    = 0.99;
 KE_h    = 0.2;
 
 %global ALK_l ALK_h
-temp_l  = 273.15+x(20); %Low latitude temperature [K] %%+15
-temp_h  = 273.15+x(21); %High latitude temperature [K] %%2.0
-salinity_l = x(23);%34.0; %low latitude salinity [g/Kg]
-salinity_h = x(24);%31.0; %high latitude salinity [g/Kg]
+temp_l  = 273.15+x(11); %Low latitude temperature [K] 
+temp_h  = 273.15+x(12); %High latitude temperature [K] 
+salinity_l = x(14);%low latitude salinity [g/Kg]
+salinity_h = x(15);%high latitude salinity [g/Kg]
 %Even though the unit of measurement are different from the one used in the
 %equations below (mmol/Kg vs mol/m3) this should not be a problem, as this
 %parameters enters only in the function co2_flux which give us a flux which
 %is moles/m2*s, which is compatible with our units.
 
 Rcp = 116.0; % carbon to phosphorus ratio (to convert export form phosphorus to carbon unit)
-Rnp = 16; % nitrogen to phosphorus ratio
-
-eps = 5.5; %isotope effect for nitrate assimilation
 
 Kwind=2/3600/24 ; % [m/s] piston velocity for air seas gas exchange///corresponding to 8.3 cm hr-1 (relatively close to 10cm hr-1 given in the Sarmiento&Gruber's book but depending on wind speed 
-
 
 %================
 % DEFINE YOUR SYSTEM
 %================
 
-dx=zeros(25,1);
-dx(1)=(-T/V_l)*x(1)+(T-KE_l*T)/V_l*x(3);
-dx(2)=(T-KE_h*T)/V_h*x(1)+(-T-M)/V_h*x(2)+(M-KE_h*M)/V_h*x(3);
-dx(3)=T/V_d*KE_h*x(1)+(T+M)/V_d*x(2)+(-M+KE_h*M+KE_l*T)/V_d*x(3);
-dx(4)=0;
-dx(5)=T/V_l*x(7)-T/V_l*x(5)-T/V_l*KE_l*Rcp*x(3)-(co2_flux(Kwind, temp_l, salinity_l,x(5), x(17), x(8),wK)*area_l)*(60*60*24*365)/V_l;
-dx(6)=T/V_h*x(5)-T/V_h*x(6)+M/V_h*x(7)-M/V_h*x(6)-T/V_h*KE_h*Rcp*x(1)-M/V_h*KE_h*Rcp*x(3)-(co2_flux(Kwind, temp_h, salinity_h,x(6), x(18), x(8),wK)*area_h)*(60*60*24*365)/V_h;
-dx(7)=T/V_d*x(6)-T/V_d*x(7)+M/V_d*x(6)-M/V_d*x(7)+T/V_d*KE_l*Rcp*x(3)+T/V_d*KE_h*Rcp*x(1)+M/V_d*KE_h*Rcp*x(3);
+dx=zeros(16,1);
+%PO4
+dx(1)= -T/V_l*x(1)+T/V_l*x(3)-T/V_l*x(3)*KE_l;
+dx(2)= T/V_h*x(1)+M/V_h*x(3)-M/V_h*x(2)-T/V_h*x(2)-T/V_h*x(1)*KE_h-M/V_h*x(3)*KE_h;
+dx(3)= T/V_d*x(2)+M/V_d*x(2)-M/V_d*x(3)-T/V_d*x(3)+T/V_d*x(3)*KE_l+T/V_d*x(1)*KE_h+M/V_d*x(3)*KE_h;
+%DIC
+dx(4)=T/V_l*x(6)-T/V_l*x(4)-T/V_l*KE_l*Rcp*x(3)-(co2_flux(Kwind, temp_l, salinity_l,x(4), x(8), x(7),wK)*area_l)*(60*60*24*365)/V_l;
+dx(5)=T/V_h*x(4)-T/V_h*x(5)+M/V_h*x(6)-M/V_h*x(5)-T/V_h*KE_h*Rcp*x(1)-M/V_h*KE_h*Rcp*x(3)-(co2_flux(Kwind, temp_h, salinity_h,x(5), x(9), x(7),wK)*area_h)*(60*60*24*365)/V_h;
+dx(6)=T/V_d*x(5)-T/V_d*x(6)+M/V_d*x(5)-M/V_d*x(6)+T/V_d*KE_l*Rcp*x(3)+T/V_d*KE_h*Rcp*x(1)+M/V_d*KE_h*Rcp*x(3);
 if isnan(setCO2)
 	%disp('Keeping ddiagnostic CO2')
-	dx(8)=((co2_flux(Kwind, temp_l, salinity_l,x(5), x(17), x(8),wK))*area_l)*(60*60*24*365)/V_a + ((co2_flux(Kwind, temp_h, salinity_h, x(6), x(18), x(8),wK))*area_h)*(60*60*24*365)/V_a;
+	dx(7)=((co2_flux(Kwind, temp_l, salinity_l,x(4), x(8), x(7),wK))*area_l)*(60*60*24*365)/V_a + ((co2_flux(Kwind, temp_h, salinity_h, x(5), x(9), x(7),wK))*area_h)*(60*60*24*365)/V_a;
  else
  	%disp('Holding CO2 levels constant at initialised initial condition')
-	dx(8)=0;
+	dx(7)=0;
 end
-	
-dx(9)=T/V_l*x(11)-T/V_l*x(9)-T/V_l*KE_l*Rnp*x(3);
-dx(10)=T/V_h*x(9)-T/V_h*x(10)+M/V_h*x(11)-M/V_h*x(10)-T/V_h*KE_h*Rnp*x(1)-M/V_h*KE_h*Rnp*x(3);
-dx(11)=T/V_d*x(10)-T/V_d*x(11)+M/V_d*x(10)-M/V_d*x(11)+T/V_d*KE_l*Rnp*x(3)+T/V_d*KE_h*Rnp*x(1)+M/V_d*KE_h*Rnp*x(3);
-dx(12)=0;
-dx(13)=T/V_l*x(15)-T/V_l*x(13)-T/V_l*KE_l*Rnp*x(3)*x(15)/x(11)*(1-(1-KE_l)^(1-(eps)/1000))/KE_l;
-dx(14)=T/V_h*x(13)-T/V_h*x(14)+M/V_h*x(15)-M/V_h*x(14)-T/V_h*KE_h*Rnp*x(1)*x(13)/x(9)*(1-(1-KE_h)^(1-(eps)/1000))/KE_h-M/V_h*KE_h*Rnp*x(3)*x(15)/x(11)*(1-(1-KE_h)^(1-(eps)/1000))/KE_h;
-dx(15)=T/V_d*x(14)-T/V_d*x(15)+M/V_d*x(14)-M/V_d*x(15)+T/V_d*KE_l*Rnp*x(3)*x(15)/x(11)*(1-(1-KE_l)^(1-(eps)/1000))/KE_l+T/V_d*KE_h*Rnp*x(1)*x(13)/x(9)*(1-(1-KE_h)^(1-(eps)/1000))/KE_h+M/V_d*KE_h*Rnp*x(3)*x(15)/x(11)*(1-(1-KE_h)^(1-(eps)/1000))/KE_h;
-dx(16)=0;
-
-dx(17)=(T)/V_l*x(19)-(T)/V_l*x(17); % needs CaCO3 and OM-acidity export fluxes
-dx(18)=(T)/V_h*x(17)+(M)/V_h*x(19)-(T+M)/V_h*x(18); % needs M-acidity export fluxe
-dx(19)=(T+M)/V_d*x(18)-(T+M)/V_d*x(19); % needs dissolution/respiration
-
-dx(20)=0; % set LL surf temp
-dx(21)=0; % set HL surf temp
-dx(22)=(T+M)/V_d*x(21)-(T+M)/V_d*x(22); % deep Sal from exchange with surface,needs checking
-
-dx(23)=0; % set LL surf Sal
-dx(24)=0; % set LL surf Sal
-dx(25)=(T+M)/V_d*x(24)-(T+M)/V_d*x(25); % deep Sal from exchange with surface,needs checking
-
+% alkalinity
+dx(8)=(T)/V_l*x(19)-(T)/V_l*x(17); % needs CaCO3 and OM-acidity export fluxes
+dx(9)=(T)/V_h*x(17)+(M)/V_h*x(19)-(T+M)/V_h*x(18); % needs M-acidity export fluxe
+dx(10)=(T+M)/V_d*x(18)-(T+M)/V_d*x(19); % needs dissolution/respiration
+%temperature
+dx(11)=0; % set LL surf temp
+dx(12)=0; % set HL surf temp
+dx(13)= T/V_d*x(12)+M/V_d*x(12)-M/V_d*x(13)-T/V_d*x(13);
+%salinity
+dx(14)=0; % set LL surf Sal
+dx(15)=0; % set LL surf Sal
+dx(16)=T/V_d*x(15)+M/V_d*x(15)-M/V_d*x(16)-T/V_d*x(16);
 % CaCO3 compensation
-
 if isnan(setSScsh)
 	%disp('No CaCO3 compensation; closed-system')	
  else
  	%disp('Holding CO2 levels constant at initialised initial condition')
-	CSH = carb_solver(x(22)+273.15,x(25),x(7), x(19), setSScsh,wK) %deep ocean carbonate chemistry
+	CSH = carb_solver(x(13)+273.15,x(16),x(6), x(10), setSScsh,wK) %deep ocean carbonate chemistry
 	netCaCO3dissolution = -(CSH-setSScsh) * (0.02/100) * (40e12)/V_d
-	dx(19)
-	
-	dx(7) = dx(7)   + 1*netCaCO3dissolution/V_d; %DIC from net CaCO3 dissolution/preservation
-	dx(19) = dx(19) + 2*netCaCO3dissolution/V_d; %ALK from net CaCO3 dissolution/preservation
+	dx(6) = dx(6)   + 1*netCaCO3dissolution/V_d; %DIC from net CaCO3 dissolution/preservation
+	dx(10) = dx(10) + 2*netCaCO3dissolution/V_d; %ALK from net CaCO3 dissolution/preservation
 end
-
 end 
 
 
